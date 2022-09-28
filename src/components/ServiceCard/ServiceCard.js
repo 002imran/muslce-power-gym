b1b6aca617800e6280b41c58bab7936753c5295f
alimranhossain <alimranhossain44@gmail.com>
2022-09-29 01:48:15 +0600

    card design, button add

--- src/components/ServiceCard/ServiceCard.js
+++ src/components/ServiceCard/ServiceCard.js
@@ -4,11 +4,14 @@ import './ServiceCard.css'
 const ServiceCard = (props) => {
     const {picture, age, time} = props.service;
     return (
-        <div className='serviceCard'>
-            <img src={picture} alt=''></img>
-            <h2>Age:{age}</h2>
-            <h3>Duration:{time} Minute</h3>
+      <div className="serviceCard">
+        <img src={picture} alt=""></img>
+        <div className='display-data'>
+          <h2>Age:{age}</h2>
+          <h3>Duration:{time} Minute</h3>
         </div>
+        <button className="button">Add to list</button>
+      </div>
     );
 };
 
