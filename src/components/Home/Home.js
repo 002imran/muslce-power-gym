317ffd3641b9ae9c829dbae7cd35860138973ebd
alimranhossain <alimranhossain44@gmail.com>
2022-09-29 02:11:57 +0600

    profile list design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ServiceCard from '../ServiceCard/ServiceCard';
+import profileImg from './faceless.jpg';
 import './Home.css'
 
 const Home = () => {
@@ -13,22 +14,36 @@ const Home = () => {
    console.log(service);
 
     return (
-         <div className='exercise-container'>
-            {/* <h1 className='heading'>Welcome To MusclePower GYM</h1> */}
-            <div className='service-container'>
-                {
-                    service.map(service=><ServiceCard
-                        key={service.id}
-                        service={service}
-                    ></ServiceCard>)
-                }
-
-            </div>
-
-            <div className="exercise-summary">
-                <h3>Profile Name</h3>
-            </div> 
+      <div className="exercise-container">
+        {/* <h1 className='heading'>Welcome To MusclePower GYM</h1> */}
+        <div className="service-container">
+          {service.map((service) => (
+            <ServiceCard key={service.id} service={service}></ServiceCard>
+          ))}
         </div>
+
+        <div className="exercise-summary">
+          <div className='profile'>
+            <img src={profileImg} alt=""></img>
+            <h2>Mr. Jhon</h2>
+          </div>
+          <div className="physical-condition">
+            <p>
+              Weight
+              <br />
+              67kg
+            </p>
+            <p>
+              Height
+              <br /> 6.2
+            </p>
+            <p>
+              Age
+              <br /> 33yrs
+            </p>
+          </div>
+        </div>
+      </div>
     );
 };
 
